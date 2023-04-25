f8d65a2a8b6f720e9a3091a99d67b711537d0ff5
satria97 <satria.code97@gmail.com>
2023-04-25 20:14:11 +0700

    Create hero section

--- components/common/header.jsx
+++ components/common/header.jsx
@@ -57,7 +57,6 @@ const Header = () => {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <Link href="#" className="-m-1.5 p-1.5">
-                {/* <span className="sr-only">Your Company</span> */}
                 <Image className="h-8 w-auto" src="/assets/logo.png" width={60} height={50} alt="" />
               </Link>
               <button
--- components/utils/hero.jsx
+++ components/utils/hero.jsx
@@ -1,40 +1,63 @@
 import { Fragment } from "react"
+import Typewriter from "typewriter-effect"
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <>
       <Fragment>
-        <section className="relative isolate px-6 pt-14">
+        <section className="relative isolate">
           <div className="mx-auto max-w-7xl flex items-center justify-between p-6 lg:px-8">
-            <div className="flex lg:flex-1">
-              <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-20">
-
+            <div className="flex lg:flex">
+              <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-24">
                 <div className="text-left">
-                  <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    Web Developer
+                  <h1 className="text-xl font-medium tracking-tight text-teal-600 sm:text-6xl">
+                    Hello🖐, I am a
+                  </h1>
+                  <h1 className="text-xl font-medium tracking-tight text-gray-900 sm:text-6xl">
+                    <Typewriter
+                      options={{
+                        strings: ['Web Developer', 'Performance Tester', 'Automation Tester'],
+                        autoStart: true,
+                        loop: true,
+                      }}
+                    />
                   </h1>
                   <p className="mt-6 text-lg leading-8 text-gray-600">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                    fugiat veniam occaecat fugiat aliqua.
+                    My name is Satria. I am a Web Developer and Quality Assurance from Landak, Indonesia
                   </p>
-                  <div className="mt-10 flex items-center justify-center gap-x-6">
-                    <a
+                  <div className="mt-10 flex items-center justify-start gap-x-6">
+                    <Link
                       href="#"
-                      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      className="rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
                     >
-                      Get started
-                    </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                      Learn more <span aria-hidden="true">→</span>
-                    </a>
+                      Contact Me
+                    </Link>
+                    <Link
+                      href="#"
+                      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Get Resume
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex lg:flex-1 bg-blue"></div>
+            <div className="flex lg:flex">
+              <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-24">
+                <div className="items-center relative">
+                  <Image src="/assets/hero.png" width={388} height={388} alt="" />
+                  <span className="absolute -bottom-0 -z-10 left-1/2 -translate-x-1/2">
+                    <svg width={400} height={400} viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                      <path fill="#08BDBA" d="M47.7,-69.7C57.9,-58.3,59.4,-39,62.1,-22.2C64.7,-5.4,68.6,9.1,66.4,23.3C64.2,37.6,56,51.7,43.9,58.6C31.8,65.6,15.9,65.5,2.6,62C-10.8,58.5,-21.6,51.6,-29.9,43.4C-38.2,35.1,-43.9,25.6,-52,13.8C-60.2,2,-70.7,-12.2,-68.4,-23.1C-66.1,-34,-51,-41.7,-37.4,-52C-23.9,-62.4,-11.9,-75.4,3.4,-80.1C18.8,-84.8,37.6,-81.2,47.7,-69.7Z" transform="translate(100 100)" />
+                    </svg>
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
-
       </Fragment>
     </>
   )

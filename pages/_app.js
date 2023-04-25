ef5f757990b468b70a5aa31a05fa7b04834ceb71
satria97 <satria.code97@gmail.com>
2023-04-25 17:32:35 +0700

    Create header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Header from '@/components/common/header'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head />
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
